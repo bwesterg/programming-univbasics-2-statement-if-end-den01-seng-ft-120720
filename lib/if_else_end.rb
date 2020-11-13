0c377ec693d0c18ea50dd650d88f65728bc05545
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-13 19:10:35 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,2 +1,9 @@
 # Write your solution herechance_of_rain = 0.2
-puts "Hello World!"
+chance_of_rain = 0.2
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter!"
+elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  puts "Pack an umbrella!"
+else
+  puts "Stay home and read Hegel."
+end
