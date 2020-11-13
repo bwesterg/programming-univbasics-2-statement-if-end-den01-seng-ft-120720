2dd5cbe5cedbacfb318d67df49903f4ac992a4df
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-13 19:36:47 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,4 +1,4 @@
 # Write your solution herechance_of_rain = 0.2
 puts "You know what year it is??"
 this_year = 2019
-puts "Hey, it's 2019!" if this_year == 2019
+puts "Hey, it's 2019!" unless this_year == 2019
